dc0f46e7a9b45badfcc05c46d16a14ef0fd2002f
Srinivasa Rao Kommanaboyina <125401403+srinivasrao85@users.noreply.github.com>
2023-07-25 21:46:39 +0530

    Update firstPipeline.groovy

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -15,7 +15,7 @@ def call(Map pipelineParams){
                         echo "printing sum of 2 numbers"
                         println cal.add(3,4)
                         println cal.multiply(3,4)
-                        echo "Microservice name is ${pipelineParams.APP_NAME}"
+                        echo "Microservice name is ${env.APP_NAME}"
                     }
                 }
             }
