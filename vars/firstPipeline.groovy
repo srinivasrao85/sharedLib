ae90612ac13decb1d9afe8edfcc4088c292d1d72
rao <rao.google.com>
2023-07-25 07:30:17 +0530

    added few lines

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,14 +1,22 @@
 import com.i27academy.builds.Calculator
-pipeline{
-    agent any
-    stages{
+def call(Map pipelineParams){
+    Calculator cal = new Calculator(this)
+    environment{
+        APP_NAME = "${pipelineParams.appName}"
+    }
+      pipeline{
+      agent any
+      stages{
         stage('echo'){
             steps{
                 script{
                     echo "printig sum of 2 numbers"
-                    println Calculator.add(3,4)
+                    println cal.add(3,4)
+                    echo "Microservice name is ${APP_NAME}"
                 }
             }
         }
     }
+}
+
 }
