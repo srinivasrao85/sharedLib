818a98b18fc4045691db1e9467a6de26caa7560f
rao <rao.google.com>
2023-07-25 21:38:14 +0530

    fixeddd

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -3,11 +3,11 @@ import com.i27academy.builds.Calculator
 def call(Map pipelineParams){
     library('com.i27academy.slb')
     Calculator cal = new Calculator(this)
-    environment{
-        APP_NAME = "${pipelineParams.appName}"
-    }
     pipeline {
         agent any
+        environment {
+        APP_NAME = "${pipelineParams.appName}"
+        }
         stages{
             stage('echo'){
                 steps{
